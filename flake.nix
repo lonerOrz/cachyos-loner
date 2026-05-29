46573dcf788b313db16377160415fa7f4645381d
loner <2788892716@qq.com>
2026-05-29 09:22:16 +0800

    refactor: remove dead let bindings from overlay, inline supportedSystems

--- flake.nix
+++ flake.nix
@@ -6,56 +6,49 @@
   outputs =
     { self, nixpkgs, ... }@inputs:
     let
-      # Common list of supported systems
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: f system);
 
-      # Helper to generate attributes for all supported systems
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      defaultOverlay =
+        final: prev:
+        let
+          projectUtils = import ./utils.nix {
+            lib = final.lib;
+            inherit nixpkgs defaultOverlay;
+          };
 
-      defaultOverlay = final: prev:
-      let
-        # Required to load version files.
-        inherit (final.lib.trivial) importJSON;
+          cachyosPackages = import ./pkgs/linux-cachyos {
+            inherit final projectUtils prev;
+            flakes = inputs;
+          };
+        in
+        {
+          inherit projectUtils;
 
-        projectUtils = import ./utils.nix { lib = final.lib; inherit nixpkgs defaultOverlay; };
-        inherit (projectUtils) multiOverride;
+          linux_cachyos = cachyosPackages.cachyos-gcc.kernel;
+          linux_cachyos-lto = cachyosPackages.cachyos-lto.kernel;
 
-        cachyosPackages = import ./pkgs/linux-cachyos {
-          inherit final projectUtils prev;
-          flakes = inputs;
+          linux_cachyos-gcc = cachyosPackages.cachyos-gcc.kernel;
+          linux_cachyos-server = cachyosPackages.cachyos-server.kernel;
+          linux_cachyos-hardened = cachyosPackages.cachyos-hardened.kernel;
+          linux_cachyos-rc = cachyosPackages.cachyos-rc.kernel;
+          linux_cachyos-lts = cachyosPackages.cachyos-lts.kernel;
+
+          linuxPackages_cachyos = cachyosPackages.cachyos-gcc;
+          linuxPackages_cachyos-lto = cachyosPackages.cachyos-lto;
+
+          linuxPackages_cachyos-gcc = cachyosPackages.cachyos-gcc;
+          linuxPackages_cachyos-server = cachyosPackages.cachyos-server;
+          linuxPackages_cachyos-hardened = cachyosPackages.cachyos-hardened;
+          linuxPackages_cachyos-rc = cachyosPackages.cachyos-rc;
+          linuxPackages_cachyos-lts = cachyosPackages.cachyos-lts;
+
+          zfs_cachyos = cachyosPackages.zfs;
         };
 
-        inherit (final.stdenv) isLinux isx86_64;
-
-      in
-      {
-        inherit projectUtils;
-
-        linux_cachyos = cachyosPackages.cachyos-gcc.kernel;
-        linux_cachyos-lto = cachyosPackages.cachyos-lto.kernel;
-
-        linux_cachyos-gcc = cachyosPackages.cachyos-gcc.kernel;
-        linux_cachyos-server = cachyosPackages.cachyos-server.kernel;
-        linux_cachyos-hardened = cachyosPackages.cachyos-hardened.kernel;
-        linux_cachyos-rc = cachyosPackages.cachyos-rc.kernel;
-        linux_cachyos-lts = cachyosPackages.cachyos-lts.kernel;
-
-        linuxPackages_cachyos = cachyosPackages.cachyos-gcc;
-        linuxPackages_cachyos-lto = cachyosPackages.cachyos-lto;
-
-        linuxPackages_cachyos-gcc = cachyosPackages.cachyos-gcc;
-        linuxPackages_cachyos-server = cachyosPackages.cachyos-server;
-        linuxPackages_cachyos-hardened = cachyosPackages.cachyos-hardened;
-        linuxPackages_cachyos-rc = cachyosPackages.cachyos-rc;
-        linuxPackages_cachyos-lts = cachyosPackages.cachyos-lts;
-
-        zfs_cachyos = cachyosPackages.zfs;
+      utils = import ./utils.nix {
+        lib = nixpkgs.lib;
+        inherit nixpkgs defaultOverlay;
       };
-
-      utils = import ./utils.nix { lib = nixpkgs.lib; inherit nixpkgs defaultOverlay; };
 
     in
     {
@@ -76,16 +69,19 @@
         }
       );
 
-      formatter = forAllSystems (system: (utils.getPkgs system).nixfmt-tree.override {
-        settings = {
-          tree-root-file = ".git/index";
-          excludes = [ ];
-          formatter.nixfmt = {
-            command = "nixfmt";
-            includes = [ "*.nix" ];
+      formatter = forAllSystems (
+        system:
+        (utils.getPkgs system).nixfmt-tree.override {
+          settings = {
+            tree-root-file = ".git/index";
+            excludes = [ ];
+            formatter.nixfmt = {
+              command = "nixfmt";
+              includes = [ "*.nix" ];
+            };
           };
-        };
-      });
+        }
+      );
 
       inherit utils;
     };
